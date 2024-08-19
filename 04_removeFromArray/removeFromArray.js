1a8fc2dda3066a2dd1537bcdc59c7e3216f9ddb4
SkyeDoesCode <160583975+SkyeDoesCode@users.noreply.github.com>
2024-08-19 13:01:11 +0200

    finished excercises 04-07

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,18 +1,12 @@
 const removeFromArray = function (array, ...args) {
-  // create a new empty array
-  const newArray = [];
-  // use forEach to go through the array
-  array.forEach((item) => {
-    // push every element into the new array
-    // UNLESS it is included in the function arguments
-    // so we create a new array with every item, except those that should be removed
-    if (!args.includes(item)) {
-      newArray.push(item);
-    }
-  });
-  // and return that array
-  return newArray;
-};
+let newArray = [];
+array.forEach((array) => {
+  if (!args.includes (array)) {
+    newArray.push(array);
+  }
 
+});
+  return newArray;
+}
 // Do not edit below this line
 module.exports = removeFromArray;
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let x = fahrenheit
+  let outputCel = (x - 32) * 5/9
+  let outputCelRounded = parseFloat(outputCel.toFixed(1))
+  return outputCelRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  let y = celsius
+  let outputFah = (y * (9/5) + 32)
+  let outputFahRounded = parseFloat(outputFah.toFixed(1))
+  return outputFahRounded
 };
 
 // Do not edit below this line
